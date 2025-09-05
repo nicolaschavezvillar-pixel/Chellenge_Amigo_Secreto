dfdfcc09178cb6c18d3318a4d716881e061f12f3
Nicolas <nicolaschavezvillar@gmail.com>
2025-09-04 23:26:51 -0400

    implementar función agregarAmigo para capturar, validar, almacenar y limpiar input

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+function agregarAmigo() {
+    let input = document.getElementById("amigo").value;
+    console.log(input); 
+}
+function agregarAmigo() {
+    let input = document.getElementById("amigo").value;
+
+    if (input.trim() === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+   
+}
+ amigos.push(input);  // Añadir el nombre al array
+ 
+ document.getElementById("amigo").value = "";  // Limpiar el campo de entrada
+
+ console.log("Lista de amigos:", amigos);
+}
